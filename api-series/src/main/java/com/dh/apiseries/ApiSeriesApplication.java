8ede81d6b80dcd5dc096d8b189a090807f02c720
IlBono <admbonesi1@gmail.com>
2022-12-12 03:15:15 -0300

    modulo estructurado de microservicio de negocio series con rabbit y evento

--- api-series/src/main/java/com/dh/apiseries/ApiSeriesApplication.java
+++ api-series/src/main/java/com/dh/apiseries/ApiSeriesApplication.java
@@ -2,8 +2,12 @@ package com.dh.apiseries;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableMongoRepositories
 public class ApiSeriesApplication {
 
 	public static void main(String[] args) {
---
+++ api-series/src/main/java/com/dh/apiseries/repository/SerieRepository.java
@@ -0,0 +1,13 @@
+package com.dh.apiseries.repository;
+
+
+import com.dh.apiseries.model.SerieEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SerieRepository extends MongoRepository<SerieEntity, Long> {
+    List<SerieEntity> findByGenre(String genre);
+}
